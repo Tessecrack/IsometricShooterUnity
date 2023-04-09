774e79c3cf9bee91cd94980e30544301120b2819
sirozu <rychen123@yandex.ru>
2023-04-09 20:14:36 +0300

    simple changes

--- Assets/Sources/Weapons/TurretWeapons/FastTurretWeapon.cs
+++ Assets/Sources/Weapons/TurretWeapons/FastTurretWeapon.cs
@@ -1,13 +1,19 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
 
 public class FastTurretWeapon : Weapon
 {
+	private ActorController owner;
+
+	private Vector3 targetPosition;
 	protected override void InitWeapon()
 	{
 		nameWeapon = "FastTurretWeapon";
 		speedAttack = 70.0f;
 		CurrentTypeWeapon = TypeWeapon.HEAVY;
-		DelayBetweenAttack = 0.4f;
+		DelayBetweenAttack = 0.8f;
 		base.InitWeapon();
 	}
 
@@ -15,8 +21,20 @@ public class FastTurretWeapon : Weapon
 	{
 		if (passedAttackTime >= DelayBetweenAttack)
 		{
-			base.StartAttack(owner, targetPosition);
+			this.owner = owner;
+			this.targetPosition = targetPosition;
+			StartCoroutine(StartFire());
 			passedAttackTime = 0;
+		}
+	}
+
+	IEnumerator StartFire()
+	{
+		foreach (var muzzle in muzzles)
+		{
+			var instanceCurrentBullet = Instantiate<Bullet>(bullet, muzzle.transform.position, muzzle.transform.rotation);
+			instanceCurrentBullet.StartFire(owner, targetPosition, speedAttack, damage);
+			yield return new WaitForFixedUpdate();
 		}
 	}
 }
