45ddb5348d3be951d3ffee6c445046db9b41bc6e
sirozu <rychen123@yandex.ru>
2023-06-14 19:46:36 +0300

    added insectoids

--- Assets/Sources/ECS/Data/Static/StaticEnemies.cs
+++ Assets/Sources/ECS/Data/Static/StaticEnemies.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 [CreateAssetMenu]
 public class StaticEnemies : ScriptableObject
 {
-	public List<GameObject> PrefabsTurret;
+	[Header("Range Enemy")]
+	public List<GameObject> PrefabsRangeEnemy;
+
+	[Header("Melee Enemy")]
+	public List<GameObject> PrefabsMeleeEnemy;
 }
