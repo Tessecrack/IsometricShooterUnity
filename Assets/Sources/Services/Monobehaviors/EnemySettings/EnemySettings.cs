e6b75836d4365b401dbb17e6ef882a1c8adffff1
sirozu <rychen123@yandex.ru>
2023-08-16 22:15:01 +0300

    only range or melee attack enemy

--- Assets/Sources/Services/Monobehaviors/EnemySettings/EnemySettings.cs
+++ Assets/Sources/Services/Monobehaviors/EnemySettings/EnemySettings.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class EnemySettings : MonoBehaviour
 {
-	[SerializeField] private bool hasArsenal;
 	[SerializeField] private bool hasMeleeAttack;
 	[SerializeField] private bool hasRangeAttack;
 	[SerializeField] private int meleeDamage = 25;
@@ -11,7 +10,6 @@ public class EnemySettings : MonoBehaviour
 	[SerializeField] private float distanceMeleeAttack = 1.5f;
 	[SerializeField] private float distanceRangeAttack = 10.0f;
 
-	public bool HasArsenal => hasArsenal;
 	public bool HasMeleeAttack => hasMeleeAttack;
 	public bool HasRangeAttack => hasRangeAttack;
 	public int MeleeDamage => meleeDamage;
