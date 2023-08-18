cdbeaf2eaf4533a2d8e855fdf33cc481935b2e17
sirozu <rychen123@yandex.ru>
2023-08-18 07:27:28 +0300

    added todo

--- Assets/Sources/Services/Monobehaviors/Weapons/Weapons/MeleeWeapon/MeleeWeapon.cs
+++ Assets/Sources/Services/Monobehaviors/Weapons/Weapons/MeleeWeapon/MeleeWeapon.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class MeleeWeapon : Weapon
 {
-	private MeleeAttack meleeAttack = new MeleeAttack();
+	private MeleeAttack meleeAttack = new();
 	public override void StartAttack(in Transform startTrasform, in Vector3 targetPosition)
 	{
 		if (!meleeAttack.IsAttackInProcess)
+		{
 			meleeAttack.StartAttack();
-		Debug.Log("YYYY");
+		}
+		
 	}
 
 	public override void StopAttack()
 	{
 		if (!meleeAttack.IsAttackInProcess)
-			meleeAttack.EndAttack();
+		{
+			// meleeAttack.EndAttack();
+		}
 	}
 }
